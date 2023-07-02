6c18ac234cbaeae687356f2cfc2886941d42d03a
Garrett John Law <85005808+GarrettJohnLaw@users.noreply.github.com>
2023-07-02 16:06:37 -0700

    removed npm audit vulnerabilities

--- src/components/Navbar/index.tsx
+++ src/components/Navbar/index.tsx
@@ -1,36 +1,30 @@
-import { Nav, Navbar, Container, NavDropdown } from "react-bootstrap";
+import { Nav, Navbar, Container, Button } from "react-bootstrap";
 
 function NavbarElement() {
   return (
     <Navbar bg="primary" expand="md">
       <Container>
         <Navbar.Brand href="#home" className="text-white">
-          Navbar Brand
+          <Nav.Link href="#home" className="text-white">
+            Garrett John Law Design
+          </Nav.Link>
         </Navbar.Brand>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
-        <Navbar.Collapse id="basic-navbar-nav">
-          <Nav className="me-auto">
-            <Nav.Link href="#home" className="text-white">
-              Home
+        <Navbar.Collapse id="basic-navbar-nav" className="justify-content-end">
+          <Nav>
+            <Nav.Link href="#link" className="text-white d-flex align-items-center">
+              Services
             </Nav.Link>
-            <Nav.Link href="#link" className="text-white">
-              Link
+            <Nav.Link href="#link" className="text-white d-flex align-items-center">
+              About
             </Nav.Link>
-            <NavDropdown title="Dropdown" className="variant-primary">
-              <NavDropdown.Item href="#action/3.1" className="text-white">
-                Dropdown Item 1
-              </NavDropdown.Item>
-              <NavDropdown.Item href="#action/3.2" className="text-white">
-                Dropdown Item 2
-              </NavDropdown.Item>
-              <NavDropdown.Item href="#action/3.3" className="text-white">
-                Dropdown Item 3
-              </NavDropdown.Item>
-              <NavDropdown.Divider />
-              <NavDropdown.Item href="#action/3.4" className="text-white">
-                Another Item
-              </NavDropdown.Item>
-            </NavDropdown>
+            <Nav.Link href="#link" className="text-white d-flex align-items-center">
+              Portfolio
+            </Nav.Link>
+            {/* <Nav.Link href="#link" className="text-white"> */}
+            {/* <Button>Contact</Button> */}
+            <Button className="btn btn-secondary">Contact</Button>
+            {/* </Nav.Link> */}
           </Nav>
         </Navbar.Collapse>
       </Container>
