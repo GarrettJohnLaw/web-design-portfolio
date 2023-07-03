4b803b173f14f8d43dfd7a8e052bbaef912901f5
Garrett John Law <85005808+GarrettJohnLaw@users.noreply.github.com>
2023-07-03 15:41:22 -0700

    portfolio coming soon

--- src/components/Navbar/index.tsx
+++ src/components/Navbar/index.tsx
@@ -1,51 +1,49 @@
-import { Nav, Navbar, Container, Button } from "react-bootstrap";
+import { Nav, Navbar } from "react-bootstrap";
 import { Link, useLocation } from "react-router-dom";
 
 function NavbarElement() {
   const location = useLocation();
 
   return (
-    <Navbar bg="primary" expand="md" className="w-100">
-      <Container>
-        <Navbar.Brand as={Link} to="/" className="text-white">
-          Garrett John Law Design
-        </Navbar.Brand>
-        <Navbar.Toggle aria-controls="basic-navbar-nav" />
-        <Navbar.Collapse id="basic-navbar-nav" className="justify-content-end">
-          <Nav>
-            <Nav.Link
-              as={Link}
-              to="/services"
-              className={`d-flex align-items-center justify-content-center text-white ${
-                location.pathname === "/services" ? "active" : ""
-              }`}
-            >
-              Services
-            </Nav.Link>
-            <Nav.Link
-              as={Link}
-              to="/about"
-              className={`d-flex align-items-center justify-content-center text-white ${
-                location.pathname === "/about" ? "active" : ""
-              }`}
-            >
-              About
-            </Nav.Link>
-            <Nav.Link
-              as={Link}
-              to="/portfolio"
-              className={`d-flex align-items-center justify-content-center text-white ${
-                location.pathname === "/portfolio" ? "active" : ""
-              }`}
-            >
-              Portfolio
-            </Nav.Link>
-            <Nav.Link as={Link} to="/contact" className="btn btn-secondary ms-3">
-              Contact
-            </Nav.Link>
-          </Nav>
-        </Navbar.Collapse>
-      </Container>
+    <Navbar expand="md" className="w-100 navbar-dark" bg="primary" collapseOnSelect>
+      <Navbar.Brand as={Link} to="/" className="text-white">
+        Garrett John Law Design
+      </Navbar.Brand>
+      <Navbar.Toggle aria-controls="basic-navbar-nav" />
+      <Navbar.Collapse id="basic-navbar-nav" className="justify-content-end bg-white">
+        <Nav className="bg-primary">
+          <Nav.Link
+            as={Link}
+            to="/services"
+            className={`d-flex align-items-center justify-content-center text-white ${
+              location.pathname === "/services" ? "active" : ""
+            }`}
+          >
+            Services
+          </Nav.Link>
+          <Nav.Link
+            as={Link}
+            to="/about"
+            className={`d-flex align-items-center justify-content-center text-white ${
+              location.pathname === "/about" ? "active" : ""
+            }`}
+          >
+            About
+          </Nav.Link>
+          <Nav.Link
+            as={Link}
+            to="/portfolio"
+            className={`d-flex align-items-center justify-content-center text-white ${
+              location.pathname === "/portfolio" ? "active" : ""
+            }`}
+          >
+            Portfolio
+          </Nav.Link>
+          <Nav.Link as={Link} to="/contact" className="btn btn-secondary ms-3">
+            Contact
+          </Nav.Link>
+        </Nav>
+      </Navbar.Collapse>
     </Navbar>
   );
 }
--- src/pages/Portfolio/index.tsx
+++ src/pages/Portfolio/index.tsx
@@ -6,38 +6,36 @@ const Home: FC = (): ReactElement => {
     <div className="p-4">
       <h1 className="text-white text-center display-4">Portfolio</h1>
       <div className="row py-4">
+        <div className="col-md-4 gallery-item mb-md-0 d-flex flex-column">
+          {/* <a href="https://www.stepheniesantilli.com/" target="_blank"> */}
+          <img className="w-100" src="./images/Portfolio/stephenie-santilli.png" />
+          {/* </a> */}
+          {/* href="https://www.stepheniesantilli.com/" */}
+          <Button target="_blank">Stephenie Santilli (Coming Soon)</Button>
+        </div>
         <div className="col-md-4 gallery-item mb-md-0 d-flex flex-column h-100">
           <a href="https://www.patrickpopeorganist.com/" target="_blank">
             <img className="w-100" src="/images/Portfolio/patrick-pope.png" />
           </a>
-          <Button href="https://www.patrickpopeorganist.com/" target="_blank">
-            Patrick Pope
-          </Button>
+          {/* href="https://www.patrickpopeorganist.com/"  */}
+          <Button target="_blank">Patrick Pope (Coming Soon)</Button>
         </div>
-        <div className="col-md-4 gallery-item mb-md-0 d-flex flex-column">
-          <a href="https://www.stepheniesantilli.com/" target="_blank">
-            <img className="w-100" src="./images/Portfolio/stephenie-santilli.png" />
-          </a>
-          <Button href="https://www.stepheniesantilli.com/" target="_blank">
-            Stephenie Santilli
-          </Button>
-        </div>
-        <div className="col-md-4 gallery-item mb-0 d-flex flex-column">
-          <a href="https://www.garrettjohnlaw.com/" target="_blank">
-            <img className="w-100" src="/images/Portfolio/garrett-law.png" />
-          </a>
-          <Button href="https://www.garrettjohnlaw.com/" target="_blank">
-            Garrett John Law
-          </Button>
-        </div>
-      </div>
-      <div className="row py-4">
         <div className="col-md-4 gallery-item mb-md-0 d-flex flex-column h-100">
           <a href="https://holytrinity.squarespace.com/" target="_blank">
             <img className="w-100" src="/images/Portfolio/holy-trinity.png" />
           </a>
           <Button href="https://holytrinity.squarespace.com/" target="_blank">
             Holy Trinity Episcopal Church
+          </Button>
+        </div>
+      </div>
+      <div className="row py-4">
+        <div className="col-md-4 gallery-item mb-0 d-flex flex-column">
+          <a href="https://www.garrettjohnlaw.com/" target="_blank">
+            <img className="w-100" src="/images/Portfolio/garrett-law.png" />
+          </a>
+          <Button href="https://www.garrettjohnlaw.com/" target="_blank">
+            Garrett John Law
           </Button>
         </div>
       </div>
