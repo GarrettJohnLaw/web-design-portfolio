1541319ab7fb5abdf6d90b15a4dd65da5a20971a
Garrett John Law <85005808+GarrettJohnLaw@users.noreply.github.com>
2023-07-02 19:44:38 -0700

    added form to contact page

--- src/pages/Contact/index.tsx
+++ src/pages/Contact/index.tsx
@@ -2,8 +2,30 @@ import { ReactElement, FC } from "react";
 
 const Home: FC = (): ReactElement => {
   return (
-    <div className="m-5">
-      <h1 className="huge-font m-5 text-white">Contact</h1>
+    <div className="m-5 d-flex flex-column align-items-center justify-content-center">
+      <div className="w-50 text-white">
+        <h2 className="text-secondary">Contact</h2>
+        <label>Name</label>
+        <input
+          className="form-control form-control-lg"
+          type="texr"
+          placeholder="name"
+          aria-label=".form-control-lg example"
+        />
+        <label>Email</label>
+        <input
+          className="form-control form-control-lg"
+          type="email"
+          placeholder="email"
+          aria-label=".form-control-lg example"
+        />
+        <label>Message</label>
+        <textarea
+          className="form-control form-control-lg"
+          placeholder="message"
+          aria-label=".form-control-lg example"
+        />
+      </div>
     </div>
   );
 };
