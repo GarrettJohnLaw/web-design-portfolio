b5eda38fd3a391499f408f159c54264958583f4a
Garrett John Law <85005808+GarrettJohnLaw@users.noreply.github.com>
2023-07-02 21:26:34 -0700

    changed hero image

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -8,11 +8,11 @@ const Home: FC = (): ReactElement => {
       <div className="container">
         <div className="row justify-content-between align-items-center py-5">
           <div className="col-12 col-md-6 pl-md-0 mb-4 mb-md-0">
-            <img className="w-100" src="/images/hero-image.jpeg" />
+            <img className="w-100" src="/images/garrett-hiking.jpeg" />
           </div>
           <div className="col-12 col-md-5">
-            <h1 className="display-4">Garrett John Law Design</h1>
-            <p className="large mb-4">I build websites for small businesses, nonprofits, and creatives</p>
+            <h1 className="display-3">Hi, I'm Garrett.</h1>
+            <p className="large mb-4">I build websites for small businesses, nonprofits, and creatives.</p>
             <Button href="/about">Learn More</Button>
           </div>
         </div>
