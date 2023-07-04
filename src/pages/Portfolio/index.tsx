bb4ccbfc88b84a0c1def436f0e1db7e34fa5f3d4
Garrett John Law <85005808+GarrettJohnLaw@users.noreply.github.com>
2023-07-03 19:53:55 -0700

    added buttons to portfolio page

--- src/pages/Portfolio/index.tsx
+++ src/pages/Portfolio/index.tsx
@@ -4,9 +4,9 @@ import { Link } from "react-router-dom";
 
 const Home: FC = (): ReactElement => {
   return (
-    <div className="p-4">
+    <div className="p-4 d-flex flex-column gap-3">
       <h1 className="text-white text-center display-4">Portfolio</h1>
-      <div className="row py-4">
+      <div className="row py-0">
         <div className="col-md-4 gallery-item mb-md-0 d-flex flex-column">
           {/* <a href="https://www.stepheniesantilli.com/" target="_blank"> */}
           <img className="w-100" src="./images/Portfolio/stephenie-santilli.png" />
@@ -14,14 +14,14 @@ const Home: FC = (): ReactElement => {
           {/* href="https://www.stepheniesantilli.com/" */}
           <Button target="_blank">Stephenie Santilli (Coming Soon)</Button>
         </div>
-        <div className="col-md-4 gallery-item mb-md-0 d-flex flex-column h-100">
+        <div className="col-md-4 gallery-item mb-md-0 d-flex flex-column">
           <a href="https://www.patrickpopeorganist.com/" target="_blank">
             <img className="w-100" src="/images/Portfolio/patrick-pope.png" />
           </a>
           {/* href="https://www.patrickpopeorganist.com/"  */}
           <Button target="_blank">Patrick Pope (Coming Soon)</Button>
         </div>
-        <div className="col-md-4 gallery-item mb-md-0 d-flex flex-column h-100">
+        <div className="col-md-4 gallery-item mb-md-0 d-flex flex-column">
           <a href="https://holytrinity.squarespace.com/" target="_blank">
             <img className="w-100" src="/images/Portfolio/holy-trinity.png" />
           </a>
@@ -30,7 +30,7 @@ const Home: FC = (): ReactElement => {
           </Button>
         </div>
       </div>
-      <div className="row py-4">
+      <div className="row py-0">
         <div className="col-md-4 gallery-item mb-0 d-flex flex-column">
           <a href="https://www.garrettjohnlaw.com/" target="_blank">
             <img className="w-100" src="/images/Portfolio/garrett-law.png" />
@@ -40,11 +40,14 @@ const Home: FC = (): ReactElement => {
           </Button>
         </div>
       </div>
-      <Link to="/get-started">
-        <button type="button" className="btn btn-outline-primary">
-          Back home
-        </button>
-      </Link>
+      <div className="d-flex justify-content-center gap-5">
+        <Link to="/services" className="btn btn-light btn-lg">
+          Services
+        </Link>
+        <Link to="/get-started" className="btn btn-secondary btn-lg">
+          Get Started
+        </Link>
+      </div>
     </div>
   );
 };
