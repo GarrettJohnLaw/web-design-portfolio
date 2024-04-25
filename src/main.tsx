33dcfbb8b54f66f30babdc8d0acfbcd580ec0ad0
Garrett John Law <85005808+GarrettJohnLaw@users.noreply.github.com>
2024-04-24 19:35:38 -0700

    remove speed insights

--- src/main.tsx
+++ src/main.tsx
@@ -2,11 +2,9 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
 import "./index.css";
-import { SpeedInsights } from "@vercel/speed-insights/next";
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
-    <SpeedInsights></SpeedInsights>
     <App />
   </React.StrictMode>
 );
